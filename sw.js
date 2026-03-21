0c4d83e02934adc840cd83fce5372b45f02d5585
ChrisSorbadere <chrissorbadere@gmail.com>
2026-03-21 19:48:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,9 @@
-const CACHE_NAME = 'suivi-cours-v1';
-const urlsToCache = [
-  '/suivi-cours/',
-  '/suivi-cours/index.html',
-  '/suivi-cours/manifest.json',
-  '/suivi-cours/icon-192.png',
-  '/suivi-cours/icon-512.png',
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+const CACHE = 'suivi-cours-v2';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/suivi-cours/'])));
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });

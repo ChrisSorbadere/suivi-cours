3e312b688c62768c6f5c25ace662d6cea6588509
ChrisSorbadere <chrissorbadere@gmail.com>
2026-03-21 19:52:04 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE = 'suivi-cours-v2';
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/suivi-cours/'])));
-});
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
